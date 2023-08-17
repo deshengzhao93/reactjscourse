af486108a40cc1f5f2e64ba5f2cc15c647cffffe
desheng <zdsdesheng@gmail.com>
2023-08-17 14:07:47 +0800

    change title

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -12,7 +12,7 @@ const NavigationBar = () => {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarResponsive">
                     <ul className="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
-                        <li className="nav-item"><a className="nav-link" href="/services">Services</a></li>
+                        <li className="nav-item"><a className="nav-link" href="/services">Service</a></li>
                         <li className="nav-item"><a className="nav-link" href="/portfolio">Portfolio</a></li>
                         <li className="nav-item"><a className="nav-link" href="/about">About</a></li>
                         <li className="nav-item"><a className="nav-link" href="/team">Team</a></li>
